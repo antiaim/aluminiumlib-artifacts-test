6c543141dd567c40e2254b8decf8cca87c313c49
antiaim <activmcx@gmail.com>
2023-12-17 21:29:35 +0000

    Add files via upload

---
+++ alumina_bg.wasm.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function ex_read_position_vec(a: number, b: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
