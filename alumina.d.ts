769d921532f35a68bdb49fcccb4b7cc3943733d1
antiaim <activmcx@gmail.com>
2023-12-17 21:35:03 +0000

    Add files via upload

--- alumina.d.ts
+++ alumina.d.ts
@@ -1,29 +1,7 @@
-declare namespace wasm_bindgen {
-	/* tslint:disable */
-	/* eslint-disable */
-	/**
-	* @param {WebAssembly.Memory} memory
-	* @param {number} ptr
-	*/
-	export function ex_read_position_vec(memory: WebAssembly.Memory, ptr: number): void;
-	
-}
-
-declare type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
-
-declare interface InitOutput {
-  readonly memory: WebAssembly.Memory;
-  readonly ex_read_position_vec: (a: number, b: number) => void;
-  readonly __wbindgen_malloc: (a: number, b: number) => number;
-  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
-}
-
+/* tslint:disable */
+/* eslint-disable */
 /**
-* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
-* for everything else, calls `WebAssembly.instantiate` directly.
-*
-* @param {InitInput | Promise<InitInput>} module_or_path
-*
-* @returns {Promise<InitOutput>}
+* @param {WebAssembly.Memory} memory
+* @param {number} ptr
 */
-declare function wasm_bindgen (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+export function ex_read_position_vec(memory: WebAssembly.Memory, ptr: number): void;
